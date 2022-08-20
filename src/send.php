8fb30bbb15a8d7feb7524cd939040a5a69f95258
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-08-20 13:13:05 +0900

    メールが送信できるようになりました

--- src/send.php
+++ src/send.php
@@ -1,21 +1,31 @@
 <?php
 
+// エラーを出力する
+// ini_set('display_errors', "On");
+
 mb_language("Japanese");
 mb_internal_encoding("UTF-8");
 
+$subject = $_POST['subject'];
 $fullname = $_POST['fullname'];
 $email = $_POST['email'];
+$tel = $_POST['tel'];
 $message = $_POST['message'];
 
 // メール本文を作成
-$mailText = '名前：'.$fullname."\r\n"
-           .'メールアドレス：'.$email."\r\n"
-           .'お問い合わせ内容：'.$message."\r\n";
+$mailText = '件名：'.$subject."\r\n"
+            .'名前：'.$fullname."\r\n"
+            .'メールアドレス：'.$email."\r\n"
+            .'電話番号：'.$tel."\r\n"
+            .'お問い合わせ内容：'.$message."\r\n";
 
-$headers = "From: qzectbumo@outlook.jp";
+$headers = [
+         "From"=>"qzectbumo@outlook.jp",
+         "Content-Transfer-Encoding"=>"8bit",
+         "Content-Type"=>"text/plain; charset=UTF-8"
+        ];
 
-$result = mb_send_mail($email, 'お問い合わせ', $mailText, $headers);
-echo $result;
+$result = mb_send_mail($email, $subject, $mailText, $headers);
 ?>
 
 
@@ -28,10 +38,20 @@ echo $result;
   </head>
   <body>
     <div class="container">
+        <?php if ($result) :?>
+
         <h1>送信完了</h1>
     
         <p>お問い合わせありがとうございます。</p>
         <p>送信が完了しました。</p>
+
+        <?php else : ?>
+
+        <h1>エラーが発生</h1>
+    
+        <p>サーバーに問い合わせてください</p>
+
+        <?php endif; ?>
     </div>
 
   </body>
